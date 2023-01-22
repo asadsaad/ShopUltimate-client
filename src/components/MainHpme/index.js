2def845b0016c00c885e4a7041dbf736ee880226
asadsaad <asadxd55558@gmail.com>
2023-01-22 21:26:50 +0500

    Initial

---
+++ src/components/MainHpme/index.js
@@ -0,0 +1,70 @@
+import { Grid } from "@mui/material";
+import React, { useEffect, useMemo, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getproductsbycatagery } from "../../redux/actions/productactions";
+import BannerArea from "./bannerarea";
+import BottomBar from "./bottomnavigation";
+import CardSlider from "./cardsslider";
+import SideTreeview from "./fsidebar";
+import Header from "./header";
+import MobileSide from "./mobileside";
+import ProductGrid from "./productgrid";
+import data from "./bgdata";
+export default function Main() {
+  const dispatch = useDispatch();
+  const [group, setgroup] = React.useState();
+  const [currentcat, setcurreentcat] = React.useState();
+
+  const catageries = useSelector((state) => state.catageries.catageries);
+
+  useEffect(() => {
+    if (!group) {
+      setgroup(catageries[Math.floor(Math.random() * catageries.length)]);
+    }
+  }, [catageries]);
+  useEffect(() => {
+    if (group) {
+      setcurreentcat(group?._id);
+    }
+  }, [group]);
+  useEffect(() => {
+    dispatch(getproductsbycatagery(currentcat ? currentcat : group?._id));
+  }, [currentcat]);
+  useEffect(() => {
+    if (!currentcat) {
+      setcurreentcat(group?._id);
+    }
+  }, []);
+
+  const bgdata = data.find((d) => d.name.toLocaleLowerCase() == group?.name.toLocaleLowerCase());
+  console.log(group?.name,data.map((d) => d.name));
+
+  return (
+    <>
+      <BannerArea
+        group={group}
+        setgroup={setgroup}
+        ct={currentcat}
+        setct={setcurreentcat}
+        bgdata={bgdata}
+      />
+      <CardSlider />
+      <Grid container spacing={2} sx={{ minHeight: "60vh",
+        borderTop:"1px solid #d0d0d0",
+     }}>
+        <Grid item lg={2} md={12}>
+          <SideTreeview cat={group} ct={currentcat} setct={setcurreentcat} />
+        </Grid>
+        <Grid item lg={10} md={12}>
+          <ProductGrid
+            group={group}
+            setgroup={setgroup}
+            ct={currentcat}
+            setct={setcurreentcat}
+          />
+        </Grid>
+      </Grid>
+      <BottomBar />
+    </>
+  );
+}
